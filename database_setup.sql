0aa1ce735a3d80786522b8c487fc4765ef64d50e
matthias <matthiasmarcano@gmail.com>
2026-04-17 17:05:40 -0400

    Update
    Updated fault description for ticket PRO-3743.

--- database_setup.sql
+++ database_setup.sql
@@ -15,5 +15,5 @@ CREATE TABLE IF NOT EXISTS tickets (
 
 INSERT INTO tickets (ticket_id, customer_name, phone_number, device_info, device_model, fault_description, repair_status, parts_cost, labor_cost) 
 VALUES 
-('PRO-3743', 'Matthias Marcano', '730-0966', 'Samsung', 'S22 Ultra', 'Busted Screen', 'Completed', 25.0, 15.0),
+('PRO-3743', 'Matthias Marcano', '730-0966', 'Samsung', 'S22 Ultra', 'Broken Screen', 'Completed', 25.0, 15.0),
 ('PRO-6665', 'Simran Seetahal', '765-4321', 'Iphone', '25 Pro Max', 'Touch screen not registering', 'In Queue', 0.0, 0.0);
